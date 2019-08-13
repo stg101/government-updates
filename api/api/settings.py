bef586707a4277ec6a9ecedb529c44504ba87e67
Ricardo <ry.yrupailla@gmail.com>
2019-08-13 14:51:40 -0500

    Add comments model

--- api/api/settings.py
+++ api/api/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'locations.apps.LocationsConfig',
+    'comments',
 ]
 
 MIDDLEWARE = [
--- api/comments/models.py
+++ api/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    authority = models.CharField(max_length=42)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
