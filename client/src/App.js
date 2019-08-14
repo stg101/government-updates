7e1a5c0ec1dd53fbd80b96c30fa8dab56d12ba34
Ricardo <ry.yrupailla@gmail.com>
2019-08-13 22:03:09 -0500

    Add emotion/core

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import React from "react";
 import { useRequestLocations } from "./redux/action-hooks";
 import { useLocations } from "./redux/selectors";
+import { Card } from "./components/ui";
 
 function App() {
   const requestLocations = useRequestLocations();
@@ -12,8 +15,38 @@ function App() {
 
   return (
     <div>
-      <h1>App</h1>
-      <div>{JSON.stringify(locations)}</div>
+      <h1 css={{ marginBottom: "40px" }}>Actualizaciones de gobierno</h1>
+      <div
+        css={{
+          display: "grid",
+          gridTemplateColumns: "30vw 1fr",
+          gridGap: "50px"
+        }}
+      >
+        <Card>
+          <h2>Regiones</h2>
+          {Object.values(locations).map(location => (
+            <div
+              css={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+                padding: "20px 0px",
+                margin: "0px",
+                height: "40px"
+              }}
+            >
+              <h3 css={{ margin: "0px" }}>{location.name}</h3>
+              <div>{location.authority}</div>
+            </div>
+          ))}
+        </Card>
+
+        <div>
+          <h2>Comentarios</h2>
+          <div>comentarios</div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import { Global } from "@emotion/core";
 import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <Global
+      styles={{
+        body: {
+          fontFamily: "'Roboto', 'sans-serif'",
+          padding: "40px",
+          margin: 0,
+          backgroundColor: "rgb(250,250,250)",
+          fontSize: "small"
+        },
+        a: { textDecoration: "inherit", color: "inherit" },
+        "h1, h2, h3, p, ul, li": { margin: 0, padding: 0 }
+      }}
+    />
     <App />
   </Provider>,
   document.getElementById("root")
