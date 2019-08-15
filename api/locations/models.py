77225d94dd99b87a0382204d77b09d6ae8eaa9dc
Ricardo <ry.yrupailla@gmail.com>
2019-08-15 13:29:00 -0500

    Add Location model

--- api/locations/models.py
+++ api/locations/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+
+    SCOPE_TYPES = (
+        ('Co', 'Country'),
+        ('Re', 'Region'),
+        ('Pr', 'Province'),
+        ('Di', 'District')
+    )
+    name = models.CharField(max_length=500)
+    authority = models.CharField(max_length=500)
+    scope = models.CharField(max_length=2, choices=SCOPE_TYPES)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
